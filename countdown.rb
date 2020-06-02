76f78fb532bc032b46771e852ff47cef72494d49
brody sullivan <brodysullivan0@gmail.com>
2020-06-02 02:11:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ end
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(seconds_left)
+  until seconds_left < 1 
+  puts "#{seconds_left} SECOND(S)!"
+  seconds_left -= 1 
+  sleep 1 
+end 
+puts "HAPPY NEW YEAR!"
+end 
